de7ff9e69e5ceebaa0c98a076853bf5b0bd2b91a
AlexisMTZARA <mario_ola13@hotmail.com>
2023-02-23 14:42:33 -0600

    Parcial 2

---
+++ Conversor.py
@@ -0,0 +1,18 @@
+#APP PARA CONVERTIR PIES A METROS
+#23/FEB
+#BRANDOM ALEXIS MARTINEZ "DUKO"
+
+from tkinter import *     #instalar la paqueteria
+from tkinter import ttk
+
+class Conversor: 
+    #tipo de constructor
+    def __init__(self, raiz):
+     raiz.title("PIES A METROS")
+     self.pies = StringVar()
+     self.metros = StringVar()
+
+#if __name__=='__main__'
+print("Este es el archivo principal.")
+print("Nada mas se mostrara esto...")
+     
---
+++ Principal.py
@@ -0,0 +1,7 @@
+from tkinter import*
+from tkinter import ttk
+from Conversor import Conversor
+
+raiz= Tk()
+Conversor()
+raiz.mainloop()
